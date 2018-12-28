683589518b4406431afff4af49d913d35449d38a
Shankar <psgganesh@gmail.com>
2018-12-28 18:03:13 +0800

    php cs fixes

--- src/Coupon.php
+++ src/Coupon.php
@@ -5,60 +5,60 @@ namespace Iwouldrathercode\SimpleCoupons;
 class Coupon
 {
     /**
-	 * Maximum length of the random string (excluding prefix and suffix).
-	 *
-	 * @var Int
-	 */
+     * Maximum length of the random string (excluding prefix and suffix).
+     *
+     * @var Int
+     */
     public $randomStringLength = 8;
 
     /**
-	 * Random string after generation.
-	 *
-	 * @var String
-	 */
+     * Random string after generation.
+     *
+     * @var String
+     */
     public $outputString;
 
     /**
-	 * Prefix to be used in the coupon code.
-	 *
-	 * @var String
-	 */
+     * Prefix to be used in the coupon code.
+     *
+     * @var String
+     */
     public $prefix;
 
     /**
-	 * Suffix to be used in the coupon code.
-	 *
-	 * @var String
-	 */
+     * Suffix to be used in the coupon code.
+     *
+     * @var String
+     */
     public $suffix;
 
     /**
-	 * Maximum number of iterations allowed while generating coupon code.
-	 *
-	 * @var Int
-	 */
+     * Maximum number of iterations allowed while generating coupon code.
+     *
+     * @var Int
+     */
     public $iterationLimit = 20;
 
     /**
-	 * Alphabets allowed when generating codes.
-	 *
-	 * @var Array
-	 */
+     * Alphabets allowed when generating codes.
+     *
+     * @var Array
+     */
     public $allowedChars = [
         'A','B','C','D','E','F','G','H','J',
         'K','L','M','N','P','Q','R','S','T',
         'U','V','W','X','Y','Z','1','2','3',
         '4','5','6','7','8','9'
-	];
+    ];
 
     /**
-	 * Alphabets denied when generating codes.
-	 *
-	 * @var Array
-	 */
-	protected $deniedChars = [
+     * Alphabets denied when generating codes.
+     *
+     * @var Array
+     */
+    protected $deniedChars = [
         'I','O','0'
-	];
+    ];
 
     public function __construct()
     {
@@ -76,7 +76,7 @@ class Coupon
 
     /**
      * Setter for adding prefix string
-     * 
+     *
      * @param Char
      * @return Object
      */
@@ -88,7 +88,7 @@ class Coupon
 
     /**
      * Setter for adding suffix string
-     * 
+     *
      * @param Char
      * @return Object
      */
@@ -100,7 +100,7 @@ class Coupon
 
     /**
      * Setter for configuring allowed characters array
-     * 
+     *
      * @param Array
      * @return Object
      */
@@ -112,7 +112,7 @@ class Coupon
 
     /**
      * Setter for configuring denied characters array
-     * 
+     *
      * @param Array
      * @return Object
      */
@@ -124,7 +124,7 @@ class Coupon
 
     /**
      * Master method to output the generated coupon code with prefixes and suffixes
-     * 
+     *
      * @return String
      */
     public function generate()
@@ -133,10 +133,12 @@ class Coupon
         $length = $this->randomStringLength;
 
         // Generate a valid coupon until all validation passes
-        while($this->validate() === false) {
+        while ($this->validate() === false) {
             
             // Recovery scenario - break from the loop if exceeded max. iterations
-            if($iterations >= $this->iterationLimit) { break; }
+            if ($iterations >= $this->iterationLimit) {
+                break;
+            }
 
             // Churn / Generates the random string
             $this->churn($length);
@@ -147,14 +149,14 @@ class Coupon
 
         // Need to substr as the output hex string of a bin random string
         // http://php.net/manual/en/function.random-bytes.php
-        $uniqiCode = substr($this->outputString, 0,$length);
+        $uniqiCode = substr($this->outputString, 0, $length);
         return $this->prefix.$uniqiCode.$this->suffix;
     }
 
     /**
-     * Generates a string using random_bytes, 
+     * Generates a string using random_bytes,
      * converts into hex using bin2hex and returns all uppercase string
-     * 
+     *
      * @return String
      */
     public function churn($length)
@@ -163,26 +165,25 @@ class Coupon
     }
 
     /**
-     * Validates if the string contains characters which are 
+     * Validates if the string contains characters which are
      * only allowed and does not contain characters which are
      * not allowed
-     * 
+     *
      * @return Boolean
      */
     public function validate()
     {
-        if(empty($this->outputString)) {
+        if (empty($this->outputString)) {
             return false;
         }
 
         $implodedAllowedChars = implode("", $this->allowedChars);
         $implodedDeniedChars = implode("", $this->deniedChars);
         
-        if(strpbrk($this->outputString, $implodedAllowedChars) !== false) {
-           return (strpbrk($implodedDeniedChars, $this->outputString) === false);
+        if (strpbrk($this->outputString, $implodedAllowedChars) !== false) {
+            return (strpbrk($implodedDeniedChars, $this->outputString) === false);
         }
 
         return false;
     }
-
 }
