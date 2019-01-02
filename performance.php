25a2a50a1ca9ad9cff5675b508c500f30333d22e
Shankar Ganesh <psgganesh@gmail.com>
2019-01-02 18:06:57 +0800

    Perf. time measured in secs.

--- performance.php
+++ performance.php
@@ -9,7 +9,7 @@ use JakubOnderka\PhpConsoleColor\ConsoleColor;
 use LucidFrame\Console\ConsoleTable;
 
 $table = new LucidFrame\Console\ConsoleTable();
-$table->addHeader('Coupons generated')->addHeader('Time elapsed (microsecs)')->addHeader('Status');
+$table->addHeader('Coupons generated')->addHeader('Time elapsed (sec.)')->addHeader('Status');
 
 function runRound($limit, $table)
 {
@@ -24,8 +24,11 @@ function runRound($limit, $table)
     }
     $endtime = microtime(true);
     $timeElapsed = $endtime - $starttime;
+    $hours = (int)($timeElapsed/60/60);
+    $minutes = (int)($timeElapsed/60)-$hours*60;
+    $seconds = (int)$timeElapsed-$hours*60*60-$minutes*60;
     if(count(array_unique($array))<count($array)) {
-        echo $coloredOutput->apply("color_1", $limit.' - '.$timeElapsed.' coupons - Duplicate coupons found!'.PHP_EOL);
+        echo $coloredOutput->apply("color_1", $limit.' - '.$seconds.' coupons - Duplicate coupons found!'.PHP_EOL);
         exit(1);
     } else {
         $table->addRow()->addColumn($limit)->addColumn($timeElapsed)->addColumn('No duplicates found!');
